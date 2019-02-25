9cf63fc6f92ff5818fc1442f2ae83c373469b40d
Loop97 <e0319186@u.nus.edu>
2019-02-25 19:31:23 +0800

    added categoy to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@
 #
 #  id          :integer          not null, primary key
 #  name        :string           default(""), not null
+#  category    :string             default(""), not null
 #  description :text             default(""), not null
 #  quantity    :integer          not null
 #  created_at  :datetime         not null
