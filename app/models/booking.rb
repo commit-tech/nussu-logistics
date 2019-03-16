b2c59455111498a88cb14cf3463b0d1cef67c2a0
NicholasCF <nicholas.cristian@yahoo.com>
2019-03-16 18:39:42 +0800

    Validate user in booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -33,6 +33,7 @@ class Booking < ApplicationRecord
 
   validates :quantity, numericality:{greater_than: 0}
   validates :item_id, presence: true
+  validates :user_id, presence: true
   validate :booking_must_be_at_least_one_hour_before
   validate :end_time_must_be_later_than_start_time
   validate :enough_items
