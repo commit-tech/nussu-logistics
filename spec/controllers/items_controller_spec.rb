6c185f7f9a769768f6e7942f80d769d2eb893c32
Loop97 <e0319186@u.nus.edu>
2019-03-16 17:23:14 +0800

    Added the test file for item model and item control

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ItemsController, type: :controller do
+  describe 'GET #index' do
+    it 'redirects when unauthenticated' do
+      get :index
+      should redirect_to new_user_session_path
+    end
+    it 'renders when authenticated' do
+      get :index
+      should respond_with :ok
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'redirects when unauthenticated' do
+      get :edit, params: { id: edit(:item).id }
+      should redirect_to(new_user_session_path)
+    end
+    it 'denies access when normal user edit the item parameters' do
+      user = edit(:item)
+      sign_in user
+      expect do
+        get :edit, params: { id: edit(:item).id }
+      end.to raise_error(CanCan::AccessDenied)
+    end
+    it 'provides access to admin' do
+      admin = edit(:item)
+      user.add_role(:admin)
+      sign_in user
+      get :edit, params: { id: create(:item).id }
+      should respond_with :ok
+    end
+  end
+
+  describe 'GET #new' do
+    it 'redirects when unauthenticated' do
+      get :new, params: { id: new(:item).id }
+      should redirect_to(new_user_session_path)
+    end
+    it 'denies access when normal user create new item' do
+      user = new(:item)
+      sign_in user
+      expect do
+        get :edit, params: { id: new(:item).id }
+      end.to raise_error(CanCan::AccessDenied)
+    end
+    it 'provides access to admin' do
+      admin = new(:item)
+      user.add_role(:admin)
+      sign_in user
+      get :new, params: { id: new(:item).id }
+      should respond_with :ok
+    end
+  end
+
+  describe 'GET #create' do
+    it 'redirects when unauthenticated' do
+      get :create, params: { id: create(:item).id }
+      should redirect_to(new_user_session_path)
+    end
+    it 'denies access when normal user create new items' do
+      user = create(:item)
+      sign_in user
+      expect do
+        get :create, params: { id: create(:item).id }
+      end.to raise_error(CanCan::AccessDenied)
+    end
+    it 'provides access to admin' do
+      admin = create(:item)
+      user.add_role(:admin)
+      sign_in user
+      get :create, params: { id: create(:item).id }
+      should respond_with :ok
+    end
+  end
+
+  describe 'GET #show' do
+    it 'redirects when unauthenticated' do
+        get :create, params: { id: create(:item).id }
+        should redirect_to(new_user_session_path)
+    end
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string           default(""), not null
+#  description :text             default(""), not null
+#  quantity    :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe item, type: :model do
+  it { should validate_presence_of(:id) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:quantity) }
+end
