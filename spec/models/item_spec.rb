6c5d4fe43d58d0df6654d70fd82be696b2155bdc
Loop97 <e0319186@u.nus.edu>
2019-03-16 19:37:16 +0800

    Fixed syntax error

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,7 +13,7 @@
 
 require 'rails_helper'
 
-RSpec.describe Item, type: :model dos
+RSpec.describe Item, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:quantity) }
