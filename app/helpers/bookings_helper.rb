c1dab2a54b9f884a11ba7713dadfc8cf12de5cd3
NicholasCF <nicholas.cristian@yahoo.com>
2019-04-07 16:06:57 +0800

    Add retrieve bookings helper

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,5 @@
 module BookingsHelper
+  def retrieve_bookings(start_time, end_time)
+    return Bookings.where("start_time <= :start_time AND end_time >= :end_time", start_time: start_time, end_time: end_time)
+  end   
 end
