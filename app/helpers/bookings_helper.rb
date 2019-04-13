0087df34b7ff3cf01cbd105630f10bd3321d15dc
NicholasCF <nicholas.cristian@yahoo.com>
2019-04-13 13:03:36 +0800

    Add rspec for booking helper and fix errors

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,7 +1,7 @@
 module BookingsHelper
   def day_bookings(date)
     times = retrieve_period(date)
-    sz = times.size
+    sz = times.length
     idx = 1;
 
     time1 = times[idx-1]
@@ -17,18 +17,17 @@ module BookingsHelper
       idx = idx + 1
     end
 
-    puts result
-
-    bookings = Booking.where("(start_time >= :start_time AND start_time <= :end_time) OR (end_time <= :end_time AND end_time >= :start_time) AND status = 1", start_time: date.to_datetime, end_time: date.to_datetime.tomorrow)
+    bookings = Booking.all.where(status: 1)
     result = result.map{|timing| [timing, retrieve_bookings(timing[0], timing[1], bookings)]}.to_h
   end  
 
   def retrieve_period(date)
-    booked_today = Booking.where("(start_time >= :start_time AND start_time <= :end_time) OR (end_time <= :end_time AND end_time >= :start_time) AND status = 1", start_time: date.to_datetime, end_time: date.to_datetime.tomorrow)
+    booked_today = Booking.where(status: 1)
+                          .where("(start_time >= :start_time AND start_time <= :end_time) OR (end_time <= :end_time AND end_time >= :start_time) AND status = 1", start_time: date.to_datetime, end_time: date.to_datetime.tomorrow)
     period_booked_today = booked_today.pluck(:start_time)
     period_booked_today = period_booked_today + booked_today.pluck(:end_time)
-    period_booked_today = period_booked_today + [date.to_datetime, date.to_datetime.tomorrow]
-    return period_booked_today.uniq.sort
+    period_booked_today = period_booked_today + [date.to_datetime.change(:offset => "+0800"), date.to_datetime.tomorrow.change(:offset => "+0800")]
+    return period_booked_today.uniq.sort.map(&:to_datetime)
   end
 
   def retrieve_bookings(start_time, end_time, bookings)
